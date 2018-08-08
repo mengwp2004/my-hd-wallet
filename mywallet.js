7b0eb819cd5be965cf262cff98c16166464abaae
bluesky_2004 <mengwp_2004@163.com>
2018-08-08 10:50:31 +0800

    create address by public key
    Signed-off-by: bluesky_2004 <mengwp_2004@163.com>

--- mywallet.js
+++ mywallet.js
@@ -1,6 +1,7 @@
 var bip39 = require('bip39')
 var hdkey = require('ethereumjs-wallet/dist/hdkey')
 var util = require('ethereumjs-util')
+var fs = require("fs")
 
 var mnemonic = bip39.generateMnemonic()
 console.log("mnemonic code:")
@@ -10,13 +11,59 @@ var seed = bip39.mnemonicToSeed(mnemonic)
 console.log("seed:")
 console.log(seed)
 
+console.log("---------------------------")
+
 var hdWallet = hdkey.fromMasterSeed(seed)
 console.log("hd wallet:")
 console.log(hdWallet)
 
-var key1 = hdWallet.derivePath("m/44'/60'/0'/0/0")
-console.log("key:")
+var privateExtend = hdWallet.privateExtendedKey()
+console.log("privateExtend:")
+console.log(privateExtend)
+
+//var publicExtend = hdWallet.publicExtendedKey()
+//console.log("publicExtend:")
+//console.log(publicExtend)
+
+console.log("---------------------------")
+
+
+var key1 = hdWallet.derivePath("m/44'/60'/0'/0")
+console.log("key1:")
 console.log(key1)
+publicExtend = key1.publicExtendedKey()
+console.log("publicExtend:")
+console.log(publicExtend)
+
+
+var key2 = key1.derivePath("0")
+console.log("key2:")
+console.log(key2)
+
+console.log("---------------------------")
+
+var passwd= "test"
+var hdWallet1 = key2.getWallet()
+var v3 = hdWallet1.toV3String(passwd)
+console.log("v3 format:")
+console.log(v3)
+
+var hdWallet1Name = hdWallet1.getV3Filename()
+console.log(hdWallet1Name)
+
+
+var fs = require("fs");
+fs.writeFile(hdWallet1Name,v3,{flag:'a',encoding:'utf-8',mode:'0666'},function(err){
+     if(err){
+         console.log("文件写入失败")
+     }else{
+         console.log("文件追加成功");
+     }
+
+}) 
+
+
+console.log("---------------------------")
 
 var address1 = util.pubToAddress(key1._hdkey._publicKey, true)
 console.log(address1)
@@ -26,4 +73,24 @@ console.log("address:")
 console.log(address1)
 
 
+console.log("---------------------------")
+
+//var hdWallet1 = hdkey.fromExtendedKey(privateExtend)
+var hdWallet1 = hdkey.fromExtendedKey(publicExtend)
+
+console.log("hd wallet1:")
+console.log(hdWallet1)
+
+
+var key2 = hdWallet1.derivePath("0")
+//var key2 = hdWallet1.deriveChild(0)
+console.log("public key2:")
+console.log(key2)
+
+address1 = util.pubToAddress(key2._hdkey._publicKey, true)
+console.log(address1)
+
+address1 = util.toChecksumAddress(address1.toString('hex'))
+console.log("address:")
+console.log(address1)
 
